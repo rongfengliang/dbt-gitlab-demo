efe183db110fbf67d0861f774dc8e1cd0f44f65d
dalongrong <1141591465@qq.com>
2018-10-22 13:47:17 +0800

    add package demo

--- models/gitlab/transform/gitlab_project_counts.sql
+++ models/gitlab/transform/gitlab_project_counts.sql
@@ -1,1 +1,11 @@
-select * from {{ref('gitlab_projectinfo')}}
+
+with projectdemo as (
+    select * from {{ref('gitlab_projectinfo')}}
+
+),
+app as (
+
+    select '{{get_versions_schema()}}' as versioninfo, 2 as id 
+)
+
+select a.*,b.versioninfo from projectdemo a join app b on a.id =b.id
